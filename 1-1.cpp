d0985781910932c086eef6174cdb916735d01755
vikke <142058047+eucarious@users.noreply.github.com>
2025-08-18 11:18:02 +0300

    parallelization exercise 1, "Hello, Threads"

---
+++ 1-1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void rolecall()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(std::rand()%6));
+    std::cout << "ready for duty! " << std::this_thread::get_id() << "\n";
+}
+
+int main()
+{
+
+    std::thread one(rolecall);
+    std::thread two(rolecall);
+    std::thread three(rolecall);
+    std::thread four(rolecall);
+
+    one.join();
+    two.join();
+    three.join();
+    four.join();
+
+    return 0;
+}
