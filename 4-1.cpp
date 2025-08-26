ca221d9ae0ff5059776e60a459ab7aa4bcb500dc
vikke <142058047+eucarious@users.noreply.github.com>
2025-08-26 11:51:22 +0300

    more inits

---
+++ 4-1.cpp
@@ -0,0 +1,7 @@
+// turning long runtime into less runtime, more memory use
+
+int main () {
+
+
+    return 0;
+}
---
+++ 4-3.cpp
@@ -0,0 +1,8 @@
+// no. NO. NOT MINIMAX AGAIN
+// anyways, utilize dynamic programming w minimax
+
+int main () {
+
+
+    return 0;
+}
